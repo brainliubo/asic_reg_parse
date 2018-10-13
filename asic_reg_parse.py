3c4179ceaf91529dba59fdae38e3b4dbbfdc785a
brainliubo <clairlb@163.com>
2018-10-13 17:19:36 +0800

    prapare work

--- asic_reg_parse.py
+++ asic_reg_parse.py
@@ -1,2 +1,12 @@
 import xlrd
 
+def read_excel():
+    workbook = xlrd.open_workbook(r'dp_cc_reg1.xls',formatting_info=True)
+    print(workbook.sheet_names()) # [u'sheet1', u'sheet2']
+    sheet = workbook.sheet_by_index(0)
+    print((sheet.cell(2,3)))
+    print(sheet.nrows,sheet.ncols)
+
+
+if __name__ == "__main__":
+    read_excel()
